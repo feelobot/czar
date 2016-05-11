f534a51f2dbc03655b8b5133c650b787ccb34688
feelobot <felix.a.rod@gmail.com>
2016-05-11 16:53:41 -0700

    fix

--- cmds/cmds.go
+++ cmds/cmds.go
@@ -2,14 +2,15 @@ package cmds
 
 import (
 	"fmt"
+	"github.com/codegangsta/cli"
 )
 
 func init() {
 	// initialization code here
 }
 
-func Ls(cmd string) {
-	fmt.Println("using ls")
+func Ls(c *cli.Context) {
+	fmt.Println("using ls", c.String("v"))
 }
 
 func Exec(cmd string) {
--- czar.go
+++ czar.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"bufio"
-	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -14,16 +12,10 @@ import (
 	"github.com/fatih/color"
 	"github.com/feelobot/czar/cmds"
 	"os"
-	"strings"
+	"strconv"
 )
 
 func main() {
-	configuration_file := fmt.Sprintf("%s/.czar.cfg.json", os.Getenv("HOME"))
-	type Configuration struct {
-		User     string
-		Tag      string
-		Metadata bool
-	}
 
 	cyan := color.New(color.FgCyan).SprintFunc()
 	yellow := color.New(color.FgYellow).SprintFunc()
@@ -35,55 +27,14 @@ func main() {
 	app.Version = "0.0.3"
 	app.Commands = []cli.Command{
 		{
-			Name:    "config",
-			Aliases: []string{"c"},
-			Usage:   "configure czar defaults",
-			Action: func(c *cli.Context) error {
-				configuration := Configuration{}
-
-				reader := bufio.NewReader(os.Stdin)
-
-				fmt.Print("Default User? (ubuntu/core/root): ")
-				user, _ := reader.ReadString('\n')
-				configuration.User = strings.TrimSpace(user)
-
-				fmt.Print("Default Tag Search Filter? (Name): ")
-				tag, _ := reader.ReadString('\n')
-				configuration.Tag = strings.TrimSpace(tag)
-				json_string, err := json.Marshal(configuration)
-				if err != nil {
-					fmt.Println(err)
-				}
-				fmt.Println(string(json_string))
-
-				config_file, err := os.Create(configuration_file)
-				if err != nil {
-					panic(err)
-				}
-				defer config_file.Close()
-				config_file.Write(json_string)
-				config_file.Close()
-				fmt.Printf("File created!")
-				return nil
-			},
-		},
-		{
 			Name:  "ls",
 			Usage: "execute commands accross ec2 instances",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "n, name",
-					Usage: "Specify instance name",
-				},
 			},
 			Action: func(c *cli.Context) {
-				var name string
 				if len(c.Args()[0]) > 0 {
 					name = c.Args()[0]
-				} else {
-					name = c.String("n")
 				}
-				cmds.Ls(name)
+				cmds.Ls(c)
 			},
 		},
 		{
@@ -110,19 +61,11 @@ func main() {
 			}, // End of Flags
 			// Execute Action
 			Action: func(c *cli.Context) error {
-				config := Configuration{}
-				if len(c.String("t")) > 0 {
-					config.Tag = c.String("t")
-				}
-				if len(c.String("u")) > 0 {
-					config.User = c.String("u")
-				}
-				config.Metadata = c.Bool("m")
-				if len(c.String("v")) > 0 && len(config.Tag) > 0 {
+				if len(c.String("v")) > 0 && len(c.String("t")) > 0 {
 					params := &ec2.DescribeInstancesInput{
 						Filters: []*ec2.Filter{
 							{ // Required
-								Name: aws.String(fmt.Sprintf("tag:%s", config.Tag)),
+								Name: aws.String(fmt.Sprintf("tag:%s", c.String("t"))),
 								Values: []*string{
 									aws.String(c.String("v")), // Required
 									// More values...
@@ -130,6 +73,7 @@ func main() {
 							},
 							// More values...
 						},
+						return nil
 					}
 					resp, err := svc.DescribeInstances(params)
 
@@ -156,16 +100,16 @@ func main() {
 					for idx, _ := range resp.Reservations {
 						for _, inst := range resp.Reservations[idx].Instances {
 							for _, tag := range inst.Tags {
-								if *tag.Key == config.Tag {
+								if *tag.Key == c.String("t") {
 									fmt.Println(fmt.Sprintf("%s:", cyan(*tag.Value)))
 									//fmt.Println(awsutil.Prettify(*inst))
-									if config.Metadata {
+									if b, err := strconv.ParseBool(c.String("m")); err != nil && b == true {
 										fmt.Println("Metadata")
 										fmt.Println(yellow(fmt.Sprintf("%s %s %s", *inst.InstanceId, *inst.PublicDnsName, *inst.PrivateIpAddress)))
 									}
 								}
 							}
-							session.Command("ssh", "-t", "-t", "-o", "StrictHostKeyChecking=no", fmt.Sprintf("%s@%s", config.User, *inst.PrivateIpAddress), fmt.Sprintf("%s", c.Args()[0])).Run()
+							session.Command("ssh", "-t", "-t", "-o", "StrictHostKeyChecking=no", fmt.Sprintf("%s@%s", c.String("u"), *inst.PrivateIpAddress), fmt.Sprintf("%s", c.Args()[0])).Run()
 
 						}
 					}
@@ -173,6 +117,5 @@ func main() {
 				return nil
 			},
 		},
+		app.Run(os.Args)
 	}
-	app.Run(os.Args)
-}
