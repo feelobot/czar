d54eb5bdf59ae250947822a854649f6287f3ce92
feelobot <felix.a.rod@gmail.com>
2016-05-12 16:20:21 -0700

    concurrency

--- cmds/cmds.go
+++ cmds/cmds.go
@@ -10,15 +10,18 @@ import (
 	"github.com/codegangsta/cli"
 	"github.com/codeskyblue/go-sh"
 	"github.com/fatih/color"
+	"runtime"
+	"sync"
 )
 
 func init() {
+	runtime.GOMAXPROCS(1)
 }
 
 func Ls(c *cli.Context) {
 	svc := ec2.New(session.New(), &aws.Config{Region: aws.String("us-east-1")})
 	cyan := color.New(color.FgCyan).SprintFunc()
-	orange := color.New(color.FgMagenta).SprintFunc()
+	magenta := color.New(color.FgMagenta).SprintFunc()
 	yellow := color.New(color.FgYellow).SprintFunc()
 	params := &ec2.DescribeInstancesInput{
 		Filters: []*ec2.Filter{
@@ -59,7 +62,7 @@ func Ls(c *cli.Context) {
 					}
 					fmt.Printf(*inst.InstanceId)
 					fmt.Printf(yellow(fmt.Sprintf(" %s ", *inst.PublicDnsName)))
-					fmt.Println(orange(fmt.Sprintf("%s", *inst.PrivateIpAddress)))
+					fmt.Println(magenta(fmt.Sprintf("%s", *inst.PrivateIpAddress)))
 				}
 			}
 		}
@@ -75,7 +78,9 @@ func Ssh(c *cli.Context) {
 	}
 	svc := ec2.New(session.New(), &aws.Config{Region: aws.String("us-east-1")})
 	cyan := color.New(color.FgCyan).SprintFunc()
+	magenta := color.New(color.FgMagenta).SprintFunc()
 	yellow := color.New(color.FgYellow).SprintFunc()
+	green := color.New(color.FgGreen).SprintFunc()
 	if len(c.String("v")) > 0 && len(c.String("t")) > 0 {
 		params := &ec2.DescribeInstancesInput{
 			Filters: []*ec2.Filter{
@@ -106,32 +111,36 @@ func Ssh(c *cli.Context) {
 			}
 		}
 
+		// Declare a wait group and set the count to two.
+
 		//Start a Shell Session
 		session := sh.NewSession()
 		session.ShowCMD = c.Bool("d")
 
 		fmt.Println(fmt.Sprintf("ReservationSets: %v", len(resp.Reservations)))
 		for idx, _ := range resp.Reservations {
+			var wg sync.WaitGroup
 			for _, inst := range resp.Reservations[idx].Instances {
 				for _, tag := range inst.Tags {
-					if *tag.Key == c.String("t") {
-						fmt.Println(fmt.Sprintf("%s:", cyan(*tag.Value)))
-						if c.Bool("d") {
-							fmt.Println(awsutil.Prettify(*inst))
+					wg.Add(1)
+					go func() {
+						if *tag.Key == c.String("t") {
+							tag := *tag.Value
+							id := *inst.InstanceId
+							dns := *inst.PublicDnsName
+							ip := *inst.PrivateIpAddress
+							output, _ := session.Command("ssh", "-q", "-t", "-t", "-o", "StrictHostKeyChecking=no", fmt.Sprintf("%s@%s", c.String("u"), *inst.PrivateIpAddress), fmt.Sprintf("%s", c.Args()[0])).Output()
+							fmt.Println(fmt.Sprintf("%s %s %s %s: \n%s", cyan(id), magenta(tag), yellow(ip), green(dns), output))
+							if c.Bool("d") {
+								fmt.Println(awsutil.Prettify(*inst))
+							}
 						}
-						if c.Bool("m") {
-							fmt.Println("Metadata")
-							fmt.Println(yellow(fmt.Sprintf("%s %s %s", *inst.InstanceId, *inst.PublicDnsName, *inst.PrivateIpAddress)))
-						}
-					}
+						wg.Done()
+					}()
 				}
-				session.Command("ssh", "-q", "-t", "-t", "-o", "StrictHostKeyChecking=no", fmt.Sprintf("%s@%s", c.String("u"), *inst.PrivateIpAddress), fmt.Sprintf("%s", c.Args()[0])).Run()
-
 			}
+			// Wait for the goroutines to finish.
+			wg.Wait()
 		}
 	}
-}
-
-func exec() {
-
 }
